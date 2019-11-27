ace283df31bd4e0e3181b6a6afc2d835d4809cc9
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2019-11-28 00:52:14 +0530

    Added Search Input Component

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native'
+import SearchInput from './components/SearchInput'
 import SearchItem from './components/SearchItem'
 
 export default function App() {
-  const [searchedSong, setSearchedText] = useState('')
   const [searchHistory, setSearchHistory] = useState([])
 
-  const searchInputHandler = (searchedSong) => {
-    setSearchedText(searchedSong) // For controlled component
-  }
-
-  const addToHistoryHandler = () => {
+  const addToHistoryHandler = (searchedSong) => {
     // Adding searched text to Search History array
     setSearchHistory(searchHistory => [...searchHistory, { key: Math.random().toString(), value: searchedSong }])
+    
     // Resetting input text after search
-    setSearchedText('') 
+    // setSearchedText('') 
+    
     console.log(`Added ${searchedSong} to History`)
   }
 
   return (
     <View style={styles.screen} >
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Search for song or artist." 
-          style={styles.inputText} 
-          onChangeText={setSearchedText}
-          value={searchedSong} 
-        />
-        <Button 
-          title="Search" 
-          onPress={addToHistoryHandler}
-        />
-      </View>
+      <SearchInput onSearch={addToHistoryHandler} />
       <View style={styles.categoryTextContainer}>
         <Text style={styles.categoryText}>Search History</Text>
       </View>
@@ -49,19 +36,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     height: '100%',
     backgroundColor: '#000'
-  },
-  inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center'
-  },
-  inputText: {
-    flex: 1,
-    color: '#fff',
-    borderBottomColor: '#fff', 
-    borderBottomWidth: 1, 
-    padding: 10,
-    marginRight: 10
   },
   categoryTextContainer: {
     padding: 10,
---
+++ components/SearchInput.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { View, TextInput, Button, StyleSheet } from 'react-native'
+
+const SearchInput = props => {
+    const [searchedSong, setSearchedText] = useState('')
+
+    const searchInputHandler = (searchedSong) => {
+        setSearchedText(searchedSong) // For controlled component
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+        <TextInput 
+            placeholder="Search for song or artist." 
+            style={styles.inputText} 
+            onChangeText={setSearchedText}
+            value={searchedSong} 
+        />
+        <Button 
+            title="Search" 
+            onPress={() => props.onSearch(searchedSong)}
+        />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center'
+    },
+    inputText: {
+        flex: 1,
+        color: '#fff',
+        borderBottomColor: '#fff', 
+        borderBottomWidth: 1, 
+        padding: 10,
+        marginRight: 10
+    }
+})
+
+export default SearchInput

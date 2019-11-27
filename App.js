72eb0aeb280a8ce6237512d2a2e5d10e90480e32
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2019-11-27 23:12:21 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native'
+import { setAutoFocusEnabled } from 'expo/build/AR'
+
+export default function App() {
+  const [searchedSong, setSearchedText] = useState('')
+  const [searchHistory, setSearchHistory] = useState([])
+
+  const searchInputHandler = (searchedSong) => {
+    setSearchedText(searchedSong) // For controlled component
+  }
+
+  const addToHistoryHandler = () => {
+    console.log(`Added ${searchedSong} to History`)
+    // Adding searched text to Search History array
+    setSearchHistory(searchHistory => [...searchHistory, searchedSong])
+    // Resetting input text after search
+    setSearchedText('') 
+  }
+
+  return (
+    <View style={styles.screen} >
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Search for song or artist." 
+          style={styles.inputText} 
+          onChangeText={setSearchedText}
+          value={searchedSong} 
+        />
+        <Button 
+          title="Search" 
+          onPress={addToHistoryHandler}
+        />
+      </View>
+      <View style={styles.categoryTextContainer}>
+        <Text style={styles.categoryText}>Search History</Text>
+      </View>
+      <ScrollView>
+        {searchHistory.map((element) => (
+          <View style={styles.listItem} key={element}>
+            <Text style={styles.listItemText}>
+              {element}
+            </Text>
+          </View>
+        ))
+      }
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+    height: '100%',
+    backgroundColor: '#000'
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  inputText: {
+    flex: 1,
+    color: '#fff',
+    borderBottomColor: '#fff', 
+    borderBottomWidth: 1, 
+    padding: 10,
+    marginRight: 10
+  },
+  listItem: {
+    padding: 10,
+    borderBottomColor: '#fff',
+    borderBottomWidth: 1
+  },
+  listItemText: {
+    color: '#fff'
+  },
+  categoryTextContainer: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#1f1f1f',
+  },
+  categoryText: {
+    color: '#fff'
+  }
+})

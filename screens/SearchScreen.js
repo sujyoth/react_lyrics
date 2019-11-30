19272433ae116bcf54ab6520b94f0bbc5ee6d799
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2019-11-30 23:28:29 +0530

    Reintegrated search input into search screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import SearchInput from '../components/SearchInput'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput } from 'react-native'
 import SearchItem from '../components/SearchItem'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { throttle, debounce } from 'throttle-debounce'
 
 const SearchScreen = props => {
-  const navigationOptions = {
-    title: 'Home'
-  }
 
+  const [searchedSong, setSearchedText] = useState('')
   const [searchHistory, setSearchHistory] = useState([])
 
-  const addToHistoryHandler = (searchedSong) => {
+  const searchInputHandler = (searchedSong) => {
+    setSearchedText(searchedSong) // For controlled component
+  }
+
+  const addToHistoryHandler = () => {
+    console.log('Searching..')
     if (searchedSong == '')
       return
     // Adding searched text to Search History array
     setSearchHistory(searchHistory => [...searchHistory, { key: Math.random().toString(), value: searchedSong }])
 
     // Resetting input text after search
-    // setSearchedText('') 
+    setSearchedText('')
 
     console.log(`Added ${searchedSong} to History`)
   }
@@ -34,9 +37,21 @@ const SearchScreen = props => {
 
   return (
     <View style={styles.screen} >
-      <SearchInput onSearch={addToHistoryHandler} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Search for song or artist"
+          style={styles.inputText}
+          onChangeText={setSearchedText}
+          value={searchedSong}
+        />
+        <TouchableOpacity activeOpacity={0.7} onPress={addToHistoryHandler}>
+          <View>
+            <Icon name="search" style={styles.searchButton} />
+          </View>
+        </TouchableOpacity>
+      </View>
       <View style={styles.categoryTextContainer}>
-            <Text style={styles.categoryText}>Search History</Text>
+        <Text style={styles.categoryText}>Search History</Text>
       </View>
       <FlatList
         data={searchHistory}
@@ -45,7 +60,7 @@ const SearchScreen = props => {
             id={itemData.item.key}
             title={itemData.item.value}
             onDelete={removeFromHistoryHandler}
-            onSelect={() => props.navigation.navigate('Lyrics', {songName: 'Hallelujah'})}
+            onSelect={() => props.navigation.navigate('Lyrics', { songName: 'Hallelujah' })}
           />
         )}
       />
@@ -58,6 +73,25 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     height: '100%',
     backgroundColor: '#192231'
+  }, inputContainer: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputText: {
+    flex: 1,
+    fontSize: 16,
+    color: '#cfd9e5',
+    borderBottomColor: '#5893df',
+    borderBottomWidth: 1,
+    padding: 5,
+    marginRight: 10
+  },
+  searchButton: {
+    padding: 5,
+    fontSize: 20,
+    color: '#5893df'
   },
   categoryTextContainer: {
     padding: 10,

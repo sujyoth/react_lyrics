9f407f8fb40b636365eb6b8d2bd91fa1edbe8217
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2019-12-08 10:26:58 +0530

    Updated imports

--- components/SearchItem.js
+++ components/SearchItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SearchItem = props => {
     return (
